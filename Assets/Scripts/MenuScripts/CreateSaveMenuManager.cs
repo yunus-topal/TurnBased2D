d6ba898c21857753c20bb1499f29cdcfeb6c660f
Yunus Emre Topal <emretopal20@gmail.com>
2025-06-21 20:22:20 +0200

    cleanup

--- Assets/Scripts/MenuScripts/CreateSaveMenuManager.cs
+++ Assets/Scripts/MenuScripts/CreateSaveMenuManager.cs
@@ -1,68 +1,70 @@
+using Models;
 using UnityEngine;
 using UnityEngine.UI;
-using Helpers;
-using Models;
 
-public class CreateSaveMenuManager : MonoBehaviour
+namespace MenuScripts
 {
-    [SerializeField] private Button backButton;
-    [SerializeField] private Button createButton;
-    [SerializeField] private GameObject saveFilePrefab;
-    [SerializeField] private Transform saveFileListContainer;
-
-    [SerializeField] private GameObject CreateSaveInput;
-
-    [SerializeField] private GameObject mainPanel;
-    [SerializeField] private GameObject savePanel;
-    private void OnEnable()
+    public class CreateSaveMenuManager : MonoBehaviour
     {
-        CreateSaveInput.SetActive(false);
-        // check if there is a valid save file in the player prefs.
-        var lastUsedSave = PlayerPrefs.GetString(Helpers.Constants.lastUsedSaveKey, string.Empty);
-        // try to load the last used save file
-        var lastSave = Helpers.SaveHelper.LoadSaveFileFromFileName(lastUsedSave);
-        if (lastSave == null)
-        {
-            backButton.gameObject.SetActive(false);
-        }
+        [SerializeField] private Button backButton;
+        [SerializeField] private Button createButton;
+        [SerializeField] private GameObject saveFilePrefab;
+        [SerializeField] private Transform saveFileListContainer;
 
-        SetupSaveScrollView();
-    }
+        [SerializeField] private GameObject CreateSaveInput;
 
-    public void CreateNewSave()
-    {
-        var saveName = CreateSaveInput.GetComponentInChildren<TMPro.TMP_InputField>().text;
-        if (string.IsNullOrEmpty(saveName))
+        [SerializeField] private GameObject mainPanel;
+        [SerializeField] private GameObject savePanel;
+        private void OnEnable()
         {
-            Debug.LogError("Save name cannot be empty.");
-            return;
-        }
-        // Create a new save file with the given name
-        var newSaveFile = new SaveFile { SaveName = saveName };
-        // Save the new save file to disk
-        Helpers.SaveHelper.SaveSaveFile(newSaveFile);
-    }
-
-    public void SetupSaveScrollView()
-    {
-        // Clear existing save file entries
-        foreach (Transform child in saveFileListContainer)
-        {
-            Destroy(child.gameObject);
-        }
-        // Get all save files
-        var saveFiles = Helpers.SaveHelper.GetAllSaveFiles();
-        // Populate the scroll view with save files
-        foreach (var saveFile in saveFiles)
-        {
-            var saveFileObject = Instantiate(saveFilePrefab, saveFileListContainer);
-            // set parent
-            var saveFileUIHelper = saveFileObject.GetComponent<SaveFileUIHelper>();
-            if (saveFileUIHelper != null)
+            CreateSaveInput.SetActive(false);
+            // check if there is a valid save file in the player prefs.
+            var lastUsedSave = PlayerPrefs.GetString(Helpers.Constants.lastUsedSaveKey, string.Empty);
+            // try to load the last used save file
+            var lastSave = Helpers.SaveHelper.LoadSaveFileFromFileName(lastUsedSave);
+            if (lastSave == null)
             {
-                saveFileUIHelper.Initialize(saveFile, mainPanel, savePanel);
+                backButton.gameObject.SetActive(false);
+            }
+
+            SetupSaveScrollView();
+        }
+
+        public void CreateNewSave()
+        {
+            var saveName = CreateSaveInput.GetComponentInChildren<TMPro.TMP_InputField>().text;
+            if (string.IsNullOrEmpty(saveName))
+            {
+                Debug.LogError("Save name cannot be empty.");
+                return;
+            }
+            // Create a new save file with the given name
+            var newSaveFile = new SaveFile { SaveName = saveName };
+            // Save the new save file to disk
+            Helpers.SaveHelper.SaveSaveFile(newSaveFile);
+        }
+
+        public void SetupSaveScrollView()
+        {
+            // Clear existing save file entries
+            foreach (Transform child in saveFileListContainer)
+            {
+                Destroy(child.gameObject);
+            }
+            // Get all save files
+            var saveFiles = Helpers.SaveHelper.GetAllSaveFiles();
+            // Populate the scroll view with save files
+            foreach (var saveFile in saveFiles)
+            {
+                var saveFileObject = Instantiate(saveFilePrefab, saveFileListContainer);
+                // set parent
+                var saveFileUIHelper = saveFileObject.GetComponent<SaveFileUIHelper>();
+                if (saveFileUIHelper != null)
+                {
+                    saveFileUIHelper.Initialize(saveFile, mainPanel, savePanel);
+                }
             }
         }
-    }
 
+    }
 }
--- Assets/Scripts/MenuScripts/SaveFileUIHelper.cs
+++ Assets/Scripts/MenuScripts/SaveFileUIHelper.cs
@@ -1,35 +1,37 @@
-using MenuScripts;
 using Models;
 using UnityEngine;
 
-public class SaveFileUIHelper : MonoBehaviour
+namespace MenuScripts
 {
-    [SerializeField] private TMPro.TextMeshProUGUI saveNameText;
-    private MainMenuManager  mainMenuManager;
-    private SaveFile SaveFile { get; set; }
-    public void Initialize(SaveFile saveFile, GameObject mainPanel, GameObject savePanel)
+    public class SaveFileUIHelper : MonoBehaviour
     {
-        mainMenuManager = FindAnyObjectByType<MainMenuManager>();
-        Debug.Log("save file name: " + saveFile.SaveName);
-        SaveFile = saveFile;
-        GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() => OnSaveFileSelected(mainPanel, savePanel));
-        saveNameText.text = saveFile.SaveName;
-        // TODO add more fields to prefab and populate here accordingly.
+        [SerializeField] private TMPro.TextMeshProUGUI saveNameText;
+        private MainMenuManager  mainMenuManager;
+        private SaveFile SaveFile { get; set; }
+        public void Initialize(SaveFile saveFile, GameObject mainPanel, GameObject savePanel)
+        {
+            mainMenuManager = FindAnyObjectByType<MainMenuManager>();
+            Debug.Log("save file name: " + saveFile.SaveName);
+            SaveFile = saveFile;
+            GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() => OnSaveFileSelected(mainPanel, savePanel));
+            saveNameText.text = saveFile.SaveName;
+            // TODO add more fields to prefab and populate here accordingly.
 
-    }
+        }
 
-    private void OnSaveFileSelected(GameObject  mainPanel, GameObject savePanel)
-    {
-        // Save the selected save file name to PlayerPrefs
-        PlayerPrefs.SetString(Helpers.Constants.lastUsedSaveKey, SaveFile.SaveName);
-        PlayerPrefs.Save();
+        private void OnSaveFileSelected(GameObject  mainPanel, GameObject savePanel)
+        {
+            // Save the selected save file name to PlayerPrefs
+            PlayerPrefs.SetString(Helpers.Constants.lastUsedSaveKey, SaveFile.SaveName);
+            PlayerPrefs.Save();
 
-        // Optionally, you can load the game or perform other actions here
-        Debug.Log($"Selected save file: {SaveFile.SaveName}");
+            // Optionally, you can load the game or perform other actions here
+            Debug.Log($"Selected save file: {SaveFile.SaveName}");
 
-        // Activate main panel, disable savePanel
-        savePanel.SetActive(false);
-        mainPanel.SetActive(true);
-        mainMenuManager.CheckSaveFiles();
+            // Activate main panel, disable savePanel
+            savePanel.SetActive(false);
+            mainPanel.SetActive(true);
+            mainMenuManager.CheckSaveFiles();
+        }
     }
 }
